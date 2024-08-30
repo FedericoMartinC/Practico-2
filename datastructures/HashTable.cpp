df8d0293c39de50f8473ffb055fc6b500b9e3827
Federico Martin <FM311721@fi365.ort.edu.uy>
2024-08-30 12:15:06 -0300

    Codigo

--- datastructures/HashTable.cpp
+++ datastructures/HashTable.cpp
@@ -11,10 +11,103 @@ using namespace std;
 class HashTable
 {
 private:
+    struct ClaveValor
+    {
+        string key;
+        int value;
+        ClaveValor(string unKey,int unValue): key(unKey) , value(unValue){}
+    };
+
+    bool* aux;
+
+    ClaveValor **arr;
+
+    int buckets;
+
+    int cantElem;
+
+    int hash(string key){
+        int h = 0;
+        for (int i = 0; i < key.length(); i++)
+            h = h + key[i];
+        return h;
+    }
+
+    int getBucket(string key,int i){
+        int n=this->hash(key);
+        return abs((n + i) % this->buckets);
+    }
+
+    void addRec(int i,string key,int value){
+        int bucket = getBucket(key, i);
+        if (arr[bucket] == NULL)
+        {
+            arr[bucket] = new ClaveValor(key, value);
+        }
+        else if (arr[bucket]->key == key)
+        {
+            arr[bucket]->value++;
+        }
+        else
+        {
+            addRec(i + 1, key, value);
+        }
+
+    }
+
+    int getRec(int i, string key)
+    {
+        int bucket = getBucket(key, i);
+        if (arr[bucket]->key == key)
+        {
+            return arr[bucket]->value;
+        }
+        else
+        {
+            return getRec(i + 1, key);
+        }
+    }
+
+    bool existRec(int i, string key)
+    {
+        int bucket = getBucket(key, i);
+        if (arr[bucket] == NULL && aux[bucket]==false)
+        {
+            return false;
+        }
+        else if (arr[bucket]->key == key)
+        {
+            return true;
+        }
+        else
+        {
+            return existRec(i + 1, key);
+        }
+    }
+
+    void removeRec (int i,string key){
+        int bucket=getBucket(key, i);
+        if(arr[bucket]->key==key){
+            ClaveValor *aBorrar=arr[bucket];
+            arr[bucket]=NULL;
+            delete aBorrar;
+            aux[bucket]=true;
+        }else{
+            removeRec(i+1,key);
+        }
+    }
+    
 public:
     HashTable(int size)
     {
-        // TODO
+        this->arr = new ClaveValor *[size]();
+        this->aux = new bool[size];
+        for (int i = 0; i <size ; i++)
+        {
+            arr[i] = NULL;
+            aux[i]=false;
+        }
+        this->buckets = size;
     }
     ~HashTable()
     {
@@ -22,19 +115,21 @@ public:
     }
     void add(string clave, int valor)
     {
-        // TODO
+        this->addRec(0,clave,valor);
     }
     void remove(string clave)
     {
-        // TODO
+        assert(this->exist(clave));
+        this->removeRec(0,clave);
     }
     int get(string clave)
     {
-        // TODO
+        assert(this->exist(clave));
+        return this->getRec(0, clave);
     }
     bool exist(string clave)
     {
-        // TODO
+        return this->existRec(0, clave);
     }
 };
 
--- main.cpp
+++ main.cpp
@@ -7,8 +7,22 @@ using namespace std;
 
 int main()
 {
+    int devuelvo=0;
     int N;
     cin >> N;
-    // HashTable * tabla = new HashTable(?);
+    HashTable* tabla= new HashTable(N);
+    for (int i=0;i<N;i++){
+        string key;
+        cin >> key;
+        if(tabla->exist(key)){
+            if(tabla->get(key)==1){
+                devuelvo++;
+            }else if (tabla->get(key)>1){
+                devuelvo--;
+            }
+        }
+        tabla->add(key,1);
+    }
+    cout << devuelvo << endl;
     return 0;
 }
